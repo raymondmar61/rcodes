c987b4c390ef56b78e24a8a23ef91cebb8c1431f
raymondmar61 <raym61@hotmail.com>
2021-07-12 21:00:37 -0700

    Introduction

---
+++ BookOfRChapter01.R
@@ -0,0 +1,9 @@
+#This is a comment
+getwd()
+#save.image() saved?
+for(myitem in 5:7){
+  cat("--BRACED AREA BEGINS--\n")
+  cat("the current item is",myitem,"\n")
+  cat("--BRACED AREA ENDS--\n\n")
+}
+
