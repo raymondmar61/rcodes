461aeb022428d85673d5303870365f00f492f2db
raymondmar61 <raym61@hotmail.com>
2021-10-09 14:50:03 -0700

    Added more lessons on infinity, null, and not a number

--- BookOfRChapter06.R
+++ BookOfRChapter06.R
@@ -1,8 +1,26 @@
 numericvector<-c(3401,Inf,3.1,-555,-Inf,43)
 numericvector #print 3401.0    Inf    3.1 -555.0   -Inf   43.0
+numericvector^100
+'''
+[1]           Inf           Inf  1.368262e+49 2.687190e+274
+[5]           Inf 2.222519e+163
+'''
+numericvector<-90000^100
+numericvector #print Inf
+negativeinfinityvector<-c(-42,565, -Inf, -Inf, Inf,-45632.3)
+negativeinfinityvector #print -42.0    565.0     -Inf     -Inf      Inf -45632.3
+mathematicaloperationinfinitevalue<-Inf*-9
+mathematicaloperationinfinitevalue #print -Inf
+dividebyinfiszero<-(-59/Inf)
+dividebyinfiszero #print 0
+dividebyzeroinfinite<-(-Inf/59)
+dividebyzeroinfinite #print -Inf
 is.infinite(x=numericvector) #print FALSE  TRUE FALSE FALSE  TRUE FALSE
 is.finite(x=numericvector) #print TRUE FALSE  TRUE  TRUE FALSE  TRUE
 numericvector==Inf #print FALSE  TRUE FALSE FALSE FALSE FALSE
+infiniterelationaloperator<-(Inf>-Inf)
+infiniterelationaloperator #print TRUE
+negativeinfinityvector==Inf #print FALSE FALSE FALSE FALSE  TRUE FALSE
 notanumbervector<-c(NaN,54.3,-2,NaN,90094.123,-Inf,55)
 notanumbervector
 '''
@@ -12,7 +30,11 @@ notanumbervector
 is.nan(x=notanumbervector) #print TRUE FALSE FALSE  TRUE FALSE FALSE FALSE
 !is.nan(x=notanumbervector) #print FALSE  TRUE  TRUE FALSE  TRUE  TRUE  TRUE
 is.nan(x=notanumbervector)|is.infinite(x=notanumbervector) #print [1]  TRUE FALSE FALSE  TRUE FALSE  TRUE FALSE
-notanumbervector[-(which(is.nan(x=notanumbervector)|is.infinite(x=notanumbervector)))] #print   54.30    -2.00 90094.12    55.00 return numbers only
+notanumbervector[-(which(is.nan(x=notanumbervector)|is.infinite(x=notanumbervector)))] #print   54.30    -2.00 90094.12    55.00 #RM:  return numbers only by convert logical values into numeric index positions to remove them with negative indexes in square brackets.  In other words, the negative is do the opposite.  which returns the index number satisfying the logic and reteurns the number associated with the index position.
+infinitecanceling<-(-Inf+Inf)
+infinitecanceling #print NaN
+mathematicaloperationnan<-NaN+1
+mathematicaloperationnan #print NaN
 missingvalues<-c("character","a",NA,"with","string",NA)
 missingvalues
 '''
@@ -36,29 +58,92 @@ missingvaluesmatrix
 '''
 is.na(x=missingvalues) #print FALSE FALSE  TRUE FALSE FALSE  TRUE
 which(x=is.na(x=missingvalues)) #print 3 6
-combonainfnan<-c(NA,5,89,Inf,NA,9.43,-2.35,NaN,2.10,-8.53,-7.58,NA,-4.58,2.01,NaN)
+combonainfnan<-c(NA,5.89,Inf,NA,9.43,-2.35,NaN,2.10,-8.53,-7.58,NA,-4.58,2.01,NaN)
 combonainfnan
 '''
- [1]    NA  5.00 89.00   Inf    NA  9.43 -2.35   NaN  2.10
-[10] -8.53 -7.58    NA -4.58  2.01   NaN
+ [1]    NA  5.89   Inf    NA  9.43 -2.35   NaN  2.10 -8.53
+[10] -7.58    NA -4.58  2.01   NaN
 '''
+is.na(x=combonainfnan) #is.na finds NA and NaN.  NA and NaN are different entities; however, numerically they are the same.  NaN is used for numeric operations.  NA is used for numeric operations and non-numeric nonnumeric operations.
+
+'''
+ [1]  TRUE FALSE FALSE  TRUE FALSE FALSE  TRUE FALSE FALSE
+[10] FALSE  TRUE FALSE FALSE  TRUE
+'''
+which(x=is.na(x=combonainfnan)) #print 1  4  7 11 14
+which(x=is.nan(x=combonainfnan)) #print 7 14
+indexpositionnaonly<-which(x=(is.na(x=combonainfnan)&!is.nan(x=combonainfnan)))
+indexpositionnaonly #print 1  4 11.  Identify NA entires by checking is.na is true and is.nan is not true.
 deletena<-na.omit(object=combonainfnan) #delete all NAs and NaNs if the elements are numeric
 deletena
 '''
- [1]  5.00 89.00   Inf  9.43 -2.35  2.10 -8.53 -7.58 -4.58
-[10]  2.01
+[1]  5.89   Inf  9.43 -2.35  2.10 -8.53 -7.58 -4.58  2.01
 attr(,"na.action")
-[1]  1  5  8 12 15
+[1]  1  4  7 11 14
 attr(,"class")
 [1] "omit"
 '''
+arithmeticcalculationna<-3+(2.1*NA)-4
+arithmeticcalculationna #print NA
+3*c(1,2,NA,6,NA,NaN) #print 3   6  NA  18  NA NaN
+NA>76 #print NA
+76>NaN #print NA
+76<NA #print NA
+emptyentityna<-c(2,4,NA,8)
+emptyentityna #print 2 4 NA 8
 emptyentitynull<-c(2,4,NULL,8)
 emptyentitynull #print 2 4 8
 c(NA,NA,NA) #print NA NA NA
-c(NULL,NULL,NULL) #print NULL
+c(NULL,NULL,NULL) #print NULL.  Interpreted as one single, unsubsettable, empty object.
 is.null(x=emptyentitynull) #print FALSE
 is.null(x=c(NULL,NULL,NULL)) #print TRUE
 is.null(x=c(NULL,NA,NULL)) #print FALSE
+optionalargument<-c("string1","string2","string3")
+is.na(optionalargument) #print FALSE FALSE FALSE
+is.null(x=optionalargument) #print FALSE
+optionalargumentna<-c(NA,NA,NA)
+is.na(x=optionalargument) #print FALSE FALSE FALSE
+optionalargumentnull<-c(NULL,NULL,NULL)
+is.null(x=optionalargumentnull) #print TRUE
+NULL+53 #print numeric(0)
+53<=NULL #print logical(0)
+53>NULL #print logical(0)
+NaN-NULL+NA/Inf #print numeric(0)
+nullist<-list(member1=c(33,1,5.2,7),member2="NA or NULL?")
+nullist
+'''
+$member1
+[1] 33.0  1.0  5.2  7.0
+
+$member2
+[1] "NA or NULL?"
+'''
+nullist$member1 #print 33.0  1.0  5.2  7.0
+nullist$member3 #print NULL
+nullist$member3<-"rob"
+nullist
+'''
+$member1
+[1] 33.0  1.0  5.2  7.0
+
+$member2
+[1] "NA or NULL?"
+
+$member3
+[1] "rob"
+'''
+nullist$member3<-NA
+nullist
+'''
+$member1
+[1] 33.0  1.0  5.2  7.0
+
+$member2
+[1] "NA or NULL?"
+
+$member3
+[1] NA
+'''
 matrixattributes<-matrix(data=1:10,nrow=5,ncol=2)
 matrixattributes
 '''
