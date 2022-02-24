a4980bded096adbd44a1709508b5680a507103da
raymondmar61 <raym61@hotmail.com>
2022-02-23 20:18:24 -0800

    Switch and loop

--- BookOfRChapter10.R
+++ BookOfRChapter10.R
@@ -135,5 +135,164 @@ replicatenumbers
 > replicatenumbers
 NULL
 '''
+simpsonscharacter <- "Lisa"
+if(simpsonscharacter=="Homer"){
+  simpsonsnumber <- 12
+} else if(simpsonscharacter=="Marge"){
+  simpsonsnumber <- 34
+} else if(simpsonscharacter=="Bart"){
+  simpsonsnumber <- 56
+} else if(simpsonscharacter=="Lisa"){
+  simpsonsnumber <- 78
+} else if(simpsonscharacter=="Maggie"){
+  simpsonsnumber <- 90
+} else {
+  simpsonsnumber <- NA
+}
+simpsonsnumber #print 78
+simpsonscharacter <- "Peter"
+simpsonsnumberswitch <- switch(EXPR=simpsonscharacter,Homer=12,Marge=34,Bart=56,Lisa=78,Maggie=90,NA)
+simpsonsnumberswitch #print NA
+positionnumber <- 3
+switchintegerversion <- switch(EXPR=positionnumber,12,34,56,78,NA)
+switchintegerversion #print 56
+positionnumberzero <- 0
+switchintegerversion <- switch(EXPR=positionnumberzero,12,34,56,78,NA)
+switchintegerversion #print NULL
+for(loopnumber in 1:6){
+  cat("loopnumber is",loopnumber,"\n")
+}
+'''
+loopnumber is 1 
+loopnumber is 2 
+loopnumber is 3 
+loopnumber is 4 
+loopnumber is 5 
+loopnumber is 6 
+'''
+vectornumbers <- c(0.4,1.1,0.34,0.55)
+for(eachvectornumbers in vectornumbers){
+  print(2*eachvectornumbers)
+  '''
+  [1] 0.8
+  [1] 2.2
+  [1] 0.68
+  [1] 1.1
+  '''
+}
+listmostdatatypes <- list(aavector=c(3.4,1),bbmatrix=matrix(1:4,nrow=2,ncol=2),ccmatrix=matrix(c(T,T,F,T,F,F),3,2),ddstring="string here",eematrix=matrix(c("red","green","blue","yellow"))) #RM: default matrix column is one column?
+listmostdatatypes
+'''
+$aavector
+[1] 3.4 1.0
 
+$bbmatrix
+     [,1] [,2]
+[1,]    1    3
+[2,]    2    4
 
+$ccmatrix
+      [,1]  [,2]
+[1,]  TRUE  TRUE
+[2,]  TRUE FALSE
+[3,] FALSE FALSE
+
+$ddstring
+[1] "string here"
+
+$eematrix
+     [,1]    
+[1,] "red"   
+[2,] "green" 
+[3,] "blue"  
+[4,] "yellow"
+'''
+listofnames <- names(listmostdatatypes)
+listofnames #print "aavector" "bbmatrix" "ccmatrix" "ddstring" "eematrix"
+is.mat <- rep(NA,length(listmostdatatypes)) #is.mat check each member is a matrix
+is.mat #print NA NA NA NA NA
+numberofrows <- is.mat
+numberofcolumns <- is.mat
+data.type <- is.mat
+numberofrows #print NA NA NA NA NA
+numberofcolumns #print NA NA NA NA NA
+data.type #print NA NA NA NA NA
+for(i in 1:length(listmostdatatypes)){
+  print(i)
+  eachlistmostdatatypes <- listmostdatatypes[[i]] #RM:  double brackets required?
+  print(eachlistmostdatatypes)
+  if(is.matrix(eachlistmostdatatypes)){
+    is.mat[i] <- "Yes"
+    numberofrows[i] <- nrow(eachlistmostdatatypes)
+    numberofcolumns[i] <- ncol(eachlistmostdatatypes)
+    data.type[i] <- class(as.vector(eachlistmostdatatypes)) #coerces the matrix into a vector with as.vector and uses the class function to find the data type of the elements
+  } else {
+    is.mat[i] <- "No"
+  }
+}
+'''
+[1] 1
+$aavector
+[1] 3.4 1.0
+
+[1] 2
+$bbmatrix
+     [,1] [,2]
+[1,]    1    3
+[2,]    2    4
+
+[1] 3
+$ccmatrix
+      [,1]  [,2]
+[1,]  TRUE  TRUE
+[2,]  TRUE FALSE
+[3,] FALSE FALSE
+
+[1] 4
+$ddstring
+[1] "string here"
+
+[1] 5
+$eematrix
+     [,1]    
+[1,] "red"   
+[2,] "green" 
+[3,] "blue"  
+[4,] "yellow"
+'''
+forloopresultsdataframe <- data.frame(listofnames,is.mat,numberofrows,numberofcolumns,data.type,stringAsFactors=FALSE) #A data frame is created from the vectors given stringAsFactors=FALSE to prevent the character string vectors being automatically converted to factors
+forloopresultsdataframe
+'''
+  listofnames is.mat numberofrows numberofcolumns data.type stringAsFactors
+1    aavector     No           NA              NA      <NA>           FALSE
+2    bbmatrix    Yes            2               2   integer           FALSE
+3    ccmatrix    Yes            3               2   logical           FALSE
+4    ddstring     No           NA              NA      <NA>           FALSE
+5    eematrix    Yes            4               1 character           FALSE
+'''
+outerloopnumbers<-5:7
+outerloopnumbers #print 5 6 7
+innerloopnumbers<-9:6
+innerloopnumbers #print 9 8 7 6
+blankmatrixna <- matrix(NA,3,4) #three row and four column matrix
+blankmatrixna
+'''
+     [,1] [,2] [,3] [,4]
+[1,]   NA   NA   NA   NA
+[2,]   NA   NA   NA   NA
+[3,]   NA   NA   NA   NA
+'''
+for(i in 1:length(outerloopnumbers)){
+  for(j in 1:length(innerloopnumbers)){
+    outerloopi <- outerloopnumbers[i]
+    innerloopj <- innerloopnumbers[j]
+    blankmatrixna[i,j] <- outerloopi*innerloopj
+  }
+}
+blankmatrixna
+'''
+     [,1] [,2] [,3] [,4]
+[1,]   45   40   35   30
+[2,]   54   48   42   36
+[3,]   63   56   49   42
+'''
