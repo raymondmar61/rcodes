e0f4d763add16cb6de67990a77606f69f1f524b5
raymondmar61 <raym61@hotmail.com>
2022-03-19 16:06:31 -0700

    Function arguments input values and matrix multiplication

--- BookOfRChapter11.R
+++ BookOfRChapter11.R
@@ -101,3 +101,275 @@ yesreturnearly <- function(){
   return(bb)
 }
 yesreturnearly() #print 2.5.  Function returns one value even with multiple return statements.  The first return is executed and the function exits.
+
+#matrixmultiplication crash course is multiply the first matrix rows by the second matrix columns then add:  first matrix row1 * second matrix column 1, first matrix row 2 * second matrix column1, first matrix row2 * second matrix column1, first matrix row2 * second matrix column2.  Number of columns first matrix equals number of rows second matrix.
+matrixmultiplicationfunction <- function(inputmatrix,multiplyingmatrix,stringnotamatrix1,stringnotamatrix2){
+  matrix.flags <- sapply(inputmatrix,FUN=is.matrix) #vector matrix.flags.  sapply is an implicit looping function.  Apply is.matrix to inputmatarix.  The result is a logical vector of equal length inputmatrix.
+  print("function output")
+  print(matrix.flags)
+  if(!any(matrix.flags)){
+    return(stringnotamatrix1) #if there are not matrices in inputmatrix, then function returns stringnotamatrix1
+  }
+  indexes <- which(matrix.flags) #get matrix member indexes
+  print(indexes)
+  counter <- 0
+  result <- list() #store results as a list
+  for(i in indexes){
+    print(i)
+    temp <- inputmatrix[[i]]
+    print(temp)
+    if(ncol(temp)==nrow(multiplyingmatrix)){
+      counter <- counter+1
+      result[[counter]] <- temp%*%multiplyingmatrix
+    }
+  }
+  if(counter==0){
+    return(stringnotamatrix2)
+  } else {
+    return(result)
+  }
+}
+listfirsttestfunction <- list(matrix(1:4,2,2),"string here is not a matrix","another string here not a matrix",matrix(1:8,2,4),matrix(1:8,4,2))
+listfirsttestfunction
+'''
+[[1]]
+     [,1] [,2]
+[1,]    1    3
+[2,]    2    4
+
+[[2]]
+[1] "string here is not a matrix"
+
+[[3]]
+[1] "another string here not a matrix"
+
+[[4]]
+     [,1] [,2] [,3] [,4]
+[1,]    1    3    5    7
+[2,]    2    4    6    8
+
+[[5]]
+     [,1] [,2]
+[1,]    1    5
+[2,]    2    6
+[3,]    3    7
+[4,]    4    8
+'''
+matrixmultiplicationfunction(inputmatrix=listfirsttestfunction,multiplyingmatrix = diag(2),stringnotamatrix1 = "no matrices in inputmatrix",stringnotamatrix2 = "matrices in inputmatrix incorrect dimensions in multiplyingmatarix")
+diag(2)
+'''
+     [,1] [,2]
+[1,]    1    0
+[2,]    0    1
+'''
+'''
+[1] "function output"
+[1]  TRUE FALSE FALSE  TRUE  TRUE
+[1] 1 4 5
+[1] 1
+     [,1] [,2]
+[1,]    1    3
+[2,]    2    4
+[1] 4
+     [,1] [,2] [,3] [,4]
+[1,]    1    3    5    7
+[2,]    2    4    6    8
+[1] 5
+     [,1] [,2]
+[1,]    1    5
+[2,]    2    6
+[3,]    3    7
+[4,]    4    8
+[[1]]
+     [,1] [,2]
+[1,]    1    3
+[2,]    2    4
+
+[[2]]
+     [,1] [,2]
+[1,]    1    5
+[2,]    2    6
+[3,]    3    7
+[4,]    4    8
+'''
+listsecondtestfunction <- list(1:4,"string here not a matrix",c(F,T,T,T),"??")
+listsecondtestfunction
+'''
+[[1]]
+[1] 1 2 3 4
+
+[[2]]
+[1] "string here not a matrix"
+
+[[3]]
+[1] FALSE  TRUE  TRUE  TRUE
+
+[[4]]
+[1] "??"
+'''
+matrixmultiplicationfunction(inputmatrix=listsecondtestfunction,multiplyingmatrix = diag(2),stringnotamatrix1 = "no matrices in inputmatrix",stringnotamatrix2 = "matrices in inputmatrix incorrect dimensions in multiplyingmatarix")
+diag(2)
+'''
+     [,1] [,2]
+[1,]    1    0
+[2,]    0    1
+'''
+'''
+[1] "function output"
+[1] FALSE FALSE FALSE FALSE
+[1] "no matrices in inputmatrix"
+'''
+listthirdtestfunction <- list(1:4,"string here not a matrix",c(F,T,T,T),"??",matrix(1:8,2,4))
+listthirdtestfunction
+'''
+[[1]]
+[1] 1 2 3 4
+
+[[2]]
+[1] "string here not a matrix"
+
+[[3]]
+[1] FALSE  TRUE  TRUE  TRUE
+
+[[4]]
+[1] "??"
+
+[[5]]
+     [,1] [,2] [,3] [,4]
+[1,]    1    3    5    7
+[2,]    2    4    6    8
+'''
+matrixmultiplicationfunction(inputmatrix=listthirdtestfunction,multiplyingmatrix = diag(2),stringnotamatrix1 = "no matrices in inputmatrix",stringnotamatrix2 = "matrices in inputmatrix incorrect dimensions in multiplyingmatarix")
+'''
+[1] "function output"
+[1] FALSE FALSE FALSE FALSE  TRUE
+[1] 5
+[1] 5
+     [,1] [,2] [,3] [,4]
+[1,]    1    3    5    7
+[2,]    2    4    6    8
+[1] "matrices in inputmatrix incorrect dimensions in multiplyingmatarix"
+'''
+matrixmultiplicationfunction(inputmatrix=list(matrix(1:6,3,2)),multiplyingmatrix = matrix(c(2,3,5,6),2,2),"","")  #Number of columns first matrix equals number of rows second matrix.  Performing matrix multiplication multiple rows first matrix by columns second matrix.
+matrix(1:6,3,2)
+'''
+     [,1] [,2]
+[1,]    1    4
+[2,]    2    5
+[3,]    3    6
+'''
+matrix(c(2,3,5,6),2,2)
+'''
+     [,1] [,2]
+[1,]    2    5
+[2,]    3    6
+'''
+'''
+[1] "function output"
+[1] TRUE
+[1] 1
+[1] 1
+     [,1] [,2]
+[1,]    1    4
+[2,]    2    5
+[3,]    3    6
+[[1]]
+     [,1] [,2]
+[1,]   14   29
+[2,]   19   40
+[3,]   24   51
+'''
+matrixmultiplicationfunction2 <- function(inputmatrix,multiplyingmatrix,stringnotamatrix1="default string no valid matrices",stringnotamatrix2=stringnotamatrix1){
+  matrix.flags <- sapply(inputmatrix,FUN=is.matrix) #vector matrix.flags.  sapply is an implicit looping function.  Apply is.matrix to inputmatarix.  The result is a logical vector of equal length inputmatrix.
+  print("function output")
+  print(matrix.flags)
+  if(!any(matrix.flags)){
+    return(stringnotamatrix1) #if there are not matrices in inputmatrix, then function returns stringnotamatrix1
+  }
+  indexes <- which(matrix.flags) #get matrix member indexes
+  print(indexes)
+  counter <- 0
+  result <- list() #store results as a list
+  for(i in indexes){
+    print(i)
+    temp <- inputmatrix[[i]]
+    print(temp)
+    if(ncol(temp)==nrow(multiplyingmatrix)){
+      counter <- counter+1
+      result[[counter]] <- temp%*%multiplyingmatrix
+    }
+  }
+  if(counter==0){
+    return(stringnotamatrix2)
+  } else {
+    return(result)
+  }
+}
+matrixmultiplicationfunction2(listsecondtestfunction,multiplyingmatrix=diag(2))
+'''
+[1] "function output"
+[1] FALSE FALSE FALSE FALSE
+[1] "default string no valid matrices"
+'''
+matrixmultiplicationfunction2(listthirdtestfunction,multiplyingmatrix=diag(2))
+'''
+[1] "function output"
+[1] FALSE FALSE FALSE FALSE  TRUE
+[1] 5
+[1] 5
+     [,1] [,2] [,3] [,4]
+[1,]    1    3    5    7
+[2,]    2    4    6    8
+[1] "default string no valid matrices"
+'''
+matrixmultiplicationfunction3 <- function(inputmatrix,multiplyingmatrix,stringnotamatrix1,stringnotamatrix2){
+  matrix.flags <- sapply(inputmatrix,FUN=is.matrix) #vector matrix.flags.  sapply is an implicit looping function.  Apply is.matrix to inputmatarix.  The result is a logical vector of equal length inputmatrix.
+  print("function output")
+  print(matrix.flags)
+  if(!any(matrix.flags)){
+    if(missing(stringnotamatrix1)){
+      return("missing function found stringnotamatrix1 variable missing argument input returned TRUE")
+    } else {
+      return(stringnotamatrix1) #if there are not matrices in inputmatrix, then function returns stringnotamatrix1
+    }
+  }
+  indexes <- which(matrix.flags) #get matrix member indexes
+  print(indexes)
+  counter <- 0
+  result <- list() #store results as a list
+  for(i in indexes){
+    print(i)
+    temp <- inputmatrix[[i]]
+    print(temp)
+    if(ncol(temp)==nrow(multiplyingmatrix)){
+      counter <- counter+1
+      result[[counter]] <- temp%*%multiplyingmatrix
+    }
+  }
+  if(counter==0){
+    if(missing(stringnotamatrix2)){
+      return("missing function found stringnotamatrix2 variable missing argument input returned TRUE")
+    } else {
+      return(stringnotamatrix2)
+    } 
+  } else {
+    return(result)
+  }
+}
+matrixmultiplicationfunction3(listsecondtestfunction,diag(2))
+'''
+[1] "function output"
+[1] FALSE FALSE FALSE FALSE
+[1] "missing function found stringnotamatrix1 variable missing argument input returned TRUE"
+'''
+matrixmultiplicationfunction3(listthirdtestfunction,diag(2))
+'''
+[1] "function output"
+[1] FALSE FALSE FALSE FALSE  TRUE
+[1] 5
+[1] 5
+     [,1] [,2] [,3] [,4]
+[1,]    1    3    5    7
+[2,]    2    4    6    8
+[1] "missing function found stringnotamatrix2 variable missing argument input returned TRUE"
+'''
