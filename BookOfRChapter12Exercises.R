2dd017cafbe4ea8f71db61c74b43355673c7a1e1
raymondmar61 <raym61@hotmail.com>
2022-04-16 19:45:11 -0700

    Exercises for time execution, time, and text progress bar

--- BookOfRChapter12Exercises.R
+++ BookOfRChapter12Exercises.R
@@ -137,3 +137,136 @@ x <- "hello"
 matrixinversionfunction(x) #return Error in matrixinversionfunction(x) : listx variable must be a list.
 x <- list()
 matrixinversionfunction(x) #return Error in matrixinversionfunction(x) : listx list is empty
+
+#Exercises 12.2
+#12.2a  Include an ellipsis in its argument list to take values for the additional argument in txtProgressBar
+fancyprogresstest <- function(n,...){
+  result <- 0
+  progressbar <- txtProgressBar(min=0,max=n,...)
+  for(i in 1:n){
+    print(result)
+    Sys.sleep(0.5)
+    setTxtProgressBar(progressbar,value=i)
+    result <- result+1
+  }
+  close(progressbar)
+  return(result)
+}
+timerstart <- Sys.time()
+fancyprogresstest(50,style=3,char="r")
+timerend <- Sys.time()
+timerend-timerstart
+'''
+  |                                                                           |   0%[1] 0
+  |rr                                                                         |   2%[1] 1
+  |rrr                                                                        |   4%[1] 2
+  |rrrr                                                                       |   6%[1] 3
+  |rrrrrr                                                                     |   8%[1] 4
+  |rrrrrrrr                                                                   |  10%[1] 5
+  |rrrrrrrrr                                                                  |  12%[1] 6
+  |rrrrrrrrrr                                                                 | 
+...  
+90%[1] 45
+  |rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr      |  92%[1] 46
+  |rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr     |  94%[1] 47
+  |rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr   |  96%[1] 48
+  |rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr |  98%[1] 49
+  |rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr| 100%
+[1] 50
+> timerend <- Sys.time()
+> timerend-timerstart
+Time difference of 25.19019 secs
+'''
+fancyprogresstestcat <- function(n,...){
+  result <- 0
+  progressbar <- txtProgressBar(min=0,max=n,...)
+  for(i in 1:n){
+    cat("print result here ",result)
+    Sys.sleep(0.5)
+    setTxtProgressBar(progressbar,value=i)
+    result <- result+1
+  }
+  close(progressbar)
+  return(result)
+}
+timerstart <- Sys.time()
+fancyprogresstestcat(50,style=3,char="r")
+timerend <- Sys.time()
+timerend-timerstart
+'''
+  |rr                                                                         | 0%print result here  1
+[1] 50
+...
+  |rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr| 100%print result here  49
+[1] 50
+> timerend <- Sys.time()
+> timerend-timerstart
+Time difference of 25.14027 secs
+'''
+#12.2b  Include a progress bar style=3 for the Fibonacci sequence
+fibonaccifunction <- function(n){
+  if(n<0){
+    warning("Warning function.  You number is negative.  Convert to positive.")
+    n <- abs(n)
+  } else if(n==0){
+    stop("Stop function.  Number ns is uninterpretable at 0.")
+  }
+  if(n==1||n==2){
+    return(1)
+    } else {
+      return(fibonaccifunction(n-1)+fibonaccifunction(n-2))
+  }
+}
+fibonaccifunctionprogressbartry <- function(numberofvectors){
+  numberofterms <- length(numberofvectors)
+  result <- rep(0,numberofterms)
+  progressbar <-  txtProgressBar(min=0,max=numberofterms,style=3,char="-")
+  for(i in 1:numberofterms){
+    attempt <- try(fibonaccifunction(numberofvectors[i]),silent=T)
+    if(class(attempt)=="try-error"){
+      result[i] <- NA
+    } else {
+      result[i] <- attempt
+    }
+    setTxtProgressBar(progressbar,value=i)
+  }
+  close(progressbar)
+  return(result)
+}
+fibonaccifunctionprogressbartry(numberofvectors = c(3,2,7,0,9,13))
+'''
+  |---------------------------------------------------------------------------| 100%
+[1]   2   1  13  NA  34 233
+'''
+timerstart <- Sys.time()
+fibonaccifunctionprogressbartry(1:35)
+timerend <- Sys.time()
+timerend-timerstart
+'''
+  |---------------------------------------------------------------------------| 100%
+ [1]       1       1       2       3       5       8      13      21      34      55
+[11]      89     144     233     377     610     987    1597    2584    4181    6765
+[21]   10946   17711   28657   46368   75025  121393  196418  317811  514229  832040
+[31] 1346269 2178309 3524578 5702887 9227465
+> timerend <- Sys.time()
+> timerend-timerstart
+Time difference of 18.74432 secs
+'''
+#12.2c  A stand-alone for loop fibonacci vector
+timerstart <- Sys.time()
+fibonaccivector <- c(1,1,rep(NA,33))
+for(i in 3:35){
+  fibonaccivector[i] <- fibonaccivector[i-2]+fibonaccivector[i-1]
+}
+fibonaccivector
+timerend <- Sys.time()
+timerend-timerstart
+'''
+ [1]       1       1       2       3       5       8      13      21      34      55
+[11]      89     144     233     377     610     987    1597    2584    4181    6765
+[21]   10946   17711   28657   46368   75025  121393  196418  317811  514229  832040
+[31] 1346269 2178309 3524578 5702887 9227465
+> timerend <- Sys.time()
+> timerend-timerstart
+Time difference of 0.00462985 secs
+'''
