dc3ca0cb22b02ca9b168c9224094aac040b4abac
raymondmar61 <raym61@hotmail.com>
2022-06-04 14:55:03 -0700

    Probability basic knowledge

---
+++ BookOfRChapter15.R
@@ -0,0 +1,8 @@
+#A probability is a number which describes the chance of making an observation or statement.  The number is between 0 and 1 inclusive expressed as a fraction.
+#An event is an outcome which can occur.  Use a probability Pr to describe a chance of an event A Pr(A).  Pr(A)=0 means event A can't occur.  Pr(A)=1 means event always occurs.
+#Roll a six-sided die.  Probability roll a 5 or 6 Pr(event) = 1/6+1/6=1/3.  Example is a frequentist or classical probability.  The relative frequency an event occurs over many identical objective trials.
+#Married.  Come home late.  Use a probability Pr to describe a chance spouse is angry event B Pr(B).  Even B isn't objectively observed.  Quantity can't be easily computed.  Assign Pr(B) a number based on past experience Pr(B)=0.5 based on personal impression and knowledge of spouse's temperament or mode which is not an impartial experiment easily reproduced for any two individuals.  Example is a Bayesian probability.  Use prior knowledge or subjective beliefs.
+#Conditional probability.  The probability one event occurs after another event already occurred.  Pr(A|B) is the probability event A occurs given event B already occurred.
+#Intersection.  Two events A and B occur simultaneously.  Mutually exclusive means two events can't occur simultaneously.
+#Union.  One of two events A or B occurs.
+#Complement.  One event A doesn't occur.
