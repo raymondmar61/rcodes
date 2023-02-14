3785c90c8140b4d4c2f38e7c35e262555ff5247a
raymondmar61 <raym61@hotmail.com>
2023-02-14 00:17:52 -0800

    Learn R-Studio with Excel thinking

---
+++ rstudioforexcelusers.R
@@ -0,0 +1,46 @@
+#Introduction to R Programming for Excel Users [Ekp2mfxQSzw]
+#Set working directory Session-->Set Working Directory-->Choose Directory or Ctrl+Shift+H
+#Load csv file to a table which is the data frame 
+titanicloaddataasdataframe <- read.csv("introductiontorprogrammingforexceluserstitanic.csv",header=TRUE)
+head(titanicloaddataasdataframe)  #Observations are rows.  Variables are columns.  891 rows excluding column header.  12 columns.
+'
+PassengerId Survived Pclass                                                Name    Sex Age SibSp Parch           Ticket    Fare
+1           1        0      3                             Braund, Mr. Owen Harris   male  22     1     0        A/5 21171  7.2500
+2           2        1      1 Cumings, Mrs. John Bradley (Florence Briggs Thayer) female  38     1     0         PC 17599 71.2833
+3           3        1      3                              Heikkinen, Miss. Laina female  26     0     0 STON/O2. 3101282  7.9250
+4           4        1      1        Futrelle, Mrs. Jacques Heath (Lily May Peel) female  35     1     0           113803 53.1000
+5           5        0      3                            Allen, Mr. William Henry   male  35     0     0           373450  8.0500
+6           6        0      3                                    Moran, Mr. James   male  NA     0     0           330877  8.4583
+Cabin Embarked SurvivedLabel FamilySize
+1              S          Died          2
+2   C85        C      Survived          2
+3              S      Survived          1
+4  C123        S      Survived          2
+5              S          Died          1
+6              Q          Died          1
+'
+#Add column or insert column if statement
+titanicloaddataasdataframe$SurvivedLabel <- ifelse(titanicloaddataasdataframe$Survived==1,"Survived","Died")
+#Add column or insert column combine columns
+titanicloaddataasdataframe$FamilySize <- 1+titanicloaddataasdataframe$SibSp+titanicloaddataasdataframe$Parch
+#View column headers and its data types.  Str stands for structure.
+str(titanicloaddataasdataframe)
+'
+data.frame:	891 obs. of  14 variables:
+ $ PassengerId  : int  1 2 3 4 5 6 7 8 9 10 ...
+ $ Survived     : int  0 1 1 1 0 0 0 0 1 1 ...
+ $ Pclass       : int  3 1 3 1 3 3 1 3 3 2 ...
+ $ Name         : Factor w/ 891 levels "Abbing, Mr. Anthony",..: 109 191 358 277 16 559 520 629 417 581 ...
+ $ Sex          : Factor w/ 2 levels "female","male": 2 1 1 1 2 2 2 2 1 1 ...
+ $ Age          : num  22 38 26 35 35 NA 54 2 27 14 ...
+ $ SibSp        : int  1 1 0 1 0 0 0 3 0 1 ...
+ $ Parch        : int  0 0 0 0 0 0 0 1 2 0 ...
+ $ Ticket       : Factor w/ 681 levels "110152","110413",..: 524 597 670 50 473 276 86 396 345 133 ...
+ $ Fare         : num  7.25 71.28 7.92 53.1 8.05 ...
+ $ Cabin        : Factor w/ 148 levels "","A10","A14",..: 1 83 1 57 1 1 131 1 1 1 ...
+ $ Embarked     : Factor w/ 4 levels "","C","Q","S": 4 2 4 4 4 3 4 4 4 2 ...
+ $ SurvivedLabel: chr  "Died" "Survived" "Survived" "Survived" ...
+ $ FamilySize   : num  2 2 1 2 1 1 1 5 3 2 ...
+'
+#titanicloaddataasdataframe$SurvivedLabel <- as.factor(titanicloaddataasdataframe$SurvivedLabel) #change data type SurvivedLabel column to factor.  chr data type is text data type in R-Studio.  Best change character chr data type to factor data type.
+
