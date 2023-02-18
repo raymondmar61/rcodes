8a68c25ae555038a577642835a2f649f064a9f6c
raymondmar61 <raym61@hotmail.com>
2023-02-17 21:18:50 -0800

    Mimic Excel pivot table and Data Analysis toolpak

--- rstudioforexcelusers.R
+++ rstudioforexcelusers.R
@@ -43,4 +43,69 @@ data.frame:	891 obs. of  14 variables:
  $ FamilySize   : num  2 2 1 2 1 1 1 5 3 2 ...
 '
 #titanicloaddataasdataframe$SurvivedLabel <- as.factor(titanicloaddataasdataframe$SurvivedLabel) #change data type SurvivedLabel column to factor.  chr data type is text data type in R-Studio.  Best change character chr data type to factor data type.
-
+#average fare for males
+mean(titanicloaddataasdataframe$Fare) #print 32.20421
+mean(titanicloaddataasdataframe$Fare[titanicloaddataasdataframe$Sex=="male"]) #print 25.52389
+median(titanicloaddataasdataframe$Fare[titanicloaddataasdataframe$Sex=="male"]) #print 10.5
+mode(titanicloaddataasdataframe$Fare[titanicloaddataasdataframe$Sex=="male"]) #print "numeric"
+#R does not have a standard in-built function to calculate mode. So we create a user function to calculate mode of a data set in R. This function takes the vector as input and gives the mode value as output.  https://www.tutorialspoint.com/r/r_mean_median_mode.htm
+getmode <- function(v) {
+    uniqv <- unique(v)
+    uniqv[which.max(tabulate(match(v, uniqv)))]
+}
+result <- getmode(titanicloaddataasdataframe$Fare)
+print(result) #print 8.05
+range(titanicloaddataasdataframe$Fare[titanicloaddataasdataframe$Sex=="male"]) #print 0.0000 512.3292
+malerowsfilterrow <- titanicloaddataasdataframe[titanicloaddataasdataframe$Sex=="male",]
+head(malerowsfilterrow)
+'
+   PassengerId Survived Pclass                           Name  Sex Age SibSp Parch    Ticket    Fare Cabin Embarked SurvivedLabel
+1            1        0      3        Braund, Mr. Owen Harris male  22     1     0 A/5 21171  7.2500              S          Died
+5            5        0      3       Allen, Mr. William Henry male  35     0     0    373450  8.0500              S          Died
+6            6        0      3               Moran, Mr. James male  NA     0     0    330877  8.4583              Q          Died
+7            7        0      1        McCarthy, Mr. Timothy J male  54     0     0     17463 51.8625   E46        S          Died
+8            8        0      3 Palsson, Master. Gosta Leonard male   2     3     1    349909 21.0750              S          Died
+13          13        0      3 Saundercock, Mr. William Henry male  20     0     0 A/5. 2151  8.0500              S          Died
+   FamilySize
+1           2
+5           1
+6           1
+7           1
+8           5
+13          1
+'
+#or use brackets to slice rows
+malerowsfilterrow[1:6,]
+#Statistics male passengers and their fares
+summary(malerowsfilterrow$Fare)
+'
+   Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+  0.000   7.896  10.500  25.524  26.550 512.329 
+'
+var(malerowsfilterrow$Fare) #print 1860.91.  Variance.
+sd(malerowsfilterrow$Fare) #print 43.13826.  Standard deviation.
+sum(malerowsfilterrow$Fare) #print 14727.29
+length(malerowsfilterrow$Fare) #print 577.  Number of rows or number of observations.
+#slice columns or column range
+malerowsfilterrow[1:6,2:4]
+'
+   Survived Pclass                           Name
+1         0      3        Braund, Mr. Owen Harris
+5         0      3       Allen, Mr. William Henry
+6         0      3               Moran, Mr. James
+7         0      1        McCarthy, Mr. Timothy J
+8         0      3 Palsson, Master. Gosta Leonard
+13        0      3 Saundercock, Mr. William Henry
+'
+malerowsfilterrow[1:6,c("Survived","Name","Age")] #c is concatenate.
+'
+   Survived                           Name Age
+1         0        Braund, Mr. Owen Harris  22
+5         0       Allen, Mr. William Henry  35
+6         0               Moran, Mr. James  NA
+7         0        McCarthy, Mr. Timothy J  54
+8         0 Palsson, Master. Gosta Leonard   2
+13        0 Saundercock, Mr. William Henry  20
+'
+#or reference column index number column number
+malerowsfilterrow[1:6,c(2,4,6)]
