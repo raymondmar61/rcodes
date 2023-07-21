9dd586dd6001f185c2abf0fc5cb82849a8f09cde
raymondmar61 <raym61@hotmail.com>
2023-07-21 12:46:32 -0700

    Introduction to R-Studio basics variables, assignment operator, data types, vectors

---
+++ ultimaterbasics1.R
@@ -0,0 +1,23 @@
+#Basics
+#Press Ctrl+Enter to run 1+2
+1+2 #print 3
+abs(-5) #print 5
+'
+Single quotes for multiple line comments
+Three examples of help
+'
+?abs
+help(abs)
+example(abs)
+variableequals = 1+2
+althyphen <- "Assignment operator is arrow Alt+-"
+escapecharacter <- "Backslash \ I quote \"Stay Hungry, Stay Foolish \" from Steve Jobs."
+vectoryears <- c(2016,2017,2018,2019,2020) #c stands for concatenate
+vectorpaymentmethods <- c("Cash","Check","Credit Card","Gift Card")
+vectorasfactor <- as.factor(c("Gas","Restaurant","Medical Insurance","Gym","Gym","Gas","Medical Insurance"))
+vectorasfactor
+'
+[1] Gas               Restaurant        Medical Insurance Gym               Gym               Gas              
+[7] Medical Insurance
+Levels: Gas Gym Medical Insurance Restaurant
+'
